9fee0505675cac08911a283dc1b1fa2ae8ca0c49
yosshi4486 <yoshiharuseijin@gmail.com>
2020-09-02 14:55:50 +0900

    feat: basic operations with tests
    Append, insert and remove with its tests.

--- Sources/FlatTree/Node.swift
+++ Sources/FlatTree/Node.swift
@@ -14,11 +14,74 @@ final class Node<ItemIdentifierType> : Hashable where ItemIdentifierType : Hasha
     let id: UUID
     
     /// The actual content
-    let item: ItemIdentifierType
+    let item: ItemIdentifierType!
+    
+    /// The index in tree.
+    var index: Int
+    
+    /// The indentation level of the node.
+    var indentationLevel: Int
+    
+    /// The parrent of the node.
+    weak var parent: Node<ItemIdentifierType>?
+    
+    /// The children of the node.
+    var children: [Node<ItemIdentifierType>] = []
+    
+    /// The value that indicates where the node needs reindexing.
+    var needsReindexing = false
+    
+    /// The last index of the substree.
+    var lastIndexOfSubtree: Int {
+        var result = index
         
-    init(item: ItemIdentifierType, id: UUID = .init()) {
+        func traverse(node: Node<ItemIdentifierType>?) {
+            guard let aNode = node else {
+                return
+            }
+            result = aNode.index
+            traverse(node: aNode.children.last)
+        }
+        // O(d) where d is depth of the subtree.
+        traverse(node: children.last)
+        
+        return result
+    }
+    
+    /// The functions marks node to indicate needs like a `setNeedsDisplay` and `setNeedsLayout`
+    func setNeedsReindexing() {
+        needsReindexing = true
+        
+        func setNeedesReindexingToRoot(node: Node<ItemIdentifierType>?) {
+            guard let aNode = node, aNode.index != -1 else { // -1 is container node.
+                return
+            }
+            aNode.needsReindexing = true
+            setNeedesReindexingToRoot(node: aNode.parent)
+        }
+        setNeedesReindexingToRoot(node: parent)
+    }
+            
+    init(item: ItemIdentifierType,
+         index: Int,
+         indentationLevel: Int,
+         id: UUID = .init(),
+         parent: Node<ItemIdentifierType>? = nil,
+         children: [Node<ItemIdentifierType>] = []) {
         self.item = item
+        self.index = index
+        self.indentationLevel = indentationLevel
         self.id = id
+        self.parent = parent
+        self.children = children
+    }
+    
+    private init() {
+        // if it is the root node of the tree, the item is not referenced from anywhere.
+        self.item = nil
+        self.index = -1
+        self.indentationLevel = -1
+        self.id = UUID()
     }
     
     func hash(into hasher: inout Hasher) {
@@ -27,6 +90,10 @@ final class Node<ItemIdentifierType> : Hashable where ItemIdentifierType : Hasha
     
     static func == (lhs: Node, rhs: Node) -> Bool {
         return lhs.id == rhs.id
+    }
+    
+    static func empty() -> Node<ItemIdentifierType> {
+        return self.init()
     }
     
 }
